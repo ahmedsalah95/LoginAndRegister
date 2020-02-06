7b5424f8f69e2522223f940381081886b454c070
ahmedsalah95 <ahmedsalah9570@gmail.com>
2020-02-06 15:33:54 +0200

    adding animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,162 +25,211 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  Animation animation, delayedAnimation, muchDelayedAnimation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(duration: Duration(seconds: 3), vsync: this);
+    animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController, curve: Curves.fastOutSlowIn));
+
+    delayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn)));
+
+    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.8, 1.0, curve: Curves.fastOutSlowIn)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Center(
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      'Hello',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.bold),
+    final double width = MediaQuery.of(context).size.width;
+    animationController.forward();
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (BuildContext context, Widget child) {
+        return Scaffold(
+          body: ListView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Transform(
+                  transform: Matrix4.translationValues(
+                      animation.value * width, 0.0, 0.0),
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            'Hello',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(30.0, 35.0, 0, 0),
+                            child: Text(
+                              'There',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 40.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(135.0, 0.0, 0, 30),
+                          child: Container(
+                            child: Text(
+                              '.',
+                              style: TextStyle(
+                                  color: Colors.green[400],
+                                  fontSize: 80.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  Container(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(30.0, 35.0, 0, 0),
-                      child: Text(
-                        'There',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(135.0, 0.0, 0, 30),
-                    child: Container(
-                      child: Text(
-                        '.',
-                        style: TextStyle(
-                            color: Colors.green[400],
-                            fontSize: 80.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 5.0),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'EMAIL',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green))),
-                  ),
-                  SizedBox(height: 20.0),
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'PASSWORD',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green))),
-                    obscureText: true,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Container(
-            alignment: Alignment(1.0, 0),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 10.0, right: 20.0),
-              child: InkWell(
-                child: Text(
-                  "Forgot password",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green,
                   ),
                 ),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 5, 20.0, 5),
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  MaterialButton(
-                    onPressed: () {},
-                    elevation: 0.0,
-                    minWidth: MediaQuery.of(context).size.width,
-                    color: Colors.green,
+              SizedBox(height: 5.0),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+                child: Transform(
+                  transform: Matrix4.translationValues(
+                      delayedAnimation.value * width, 0, 0),
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'EMAIL',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.green))),
+                        ),
+                        SizedBox(height: 20.0),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'PASSWORD',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.green))),
+                          obscureText: true,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                alignment: Alignment(1.0, 0),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+                  child: InkWell(
                     child: Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  MaterialButton(
-                    onPressed: () {},
-                    elevation: 0.5,
-                    minWidth: MediaQuery.of(context).size.width,
-                    color: Colors.white,
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.fingerprint,
-                        color: Colors.black,
+                      "Forgot password",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
                       ),
-                      title: Text('Login with Facebook'),
                     ),
                   ),
-
-                ],
+                ),
               ),
-            ),
-          ),
-          SizedBox(height: 10.0,),
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Need to spotify?",
-                  style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 5, 20.0, 5),
+                child: Transform(
+                  transform: Matrix4.translationValues(
+                      muchDelayedAnimation.value * width, 0, 0),
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        MaterialButton(
+                          onPressed: () {},
+                          elevation: 0.0,
+                          minWidth: MediaQuery.of(context).size.width,
+                          color: Colors.green,
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        MaterialButton(
+                          onPressed: () {},
+                          elevation: 0.5,
+                          minWidth: MediaQuery.of(context).size.width,
+                          color: Colors.white,
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.fingerprint,
+                              color: Colors.black,
+                            ),
+                            title: Text('Login with Facebook'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  GestureDetector(
-                    onTap: (){},
-                      child: Text(" Register",style: TextStyle(color: Colors.green),))
-                ],
+                ),
               ),
-            ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Need to spotify?",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            " Register",
+                            style: TextStyle(color: Colors.green),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              )
+            ],
           ),
-          SizedBox(height: 10.0,)
-        ],
-      ),
+        );
+      },
     );
   }
 }
