3fb8fa34f7564a69274ac530261b7ebba29ebbd8
ahmedsalah95 <ahmedsalah9570@gmail.com>
2020-02-06 14:27:22 +0200

    adding buttons

---
+++ lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login and Signup',
+      theme: ThemeData(),
+      home: MyHomePage(title: 'Login and Signup'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Center(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(30.0, 35.0, 0, 0),
+                      child: Text(
+                        'There',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(135.0, 0.0, 0, 30),
+                    child: Container(
+                      child: Text(
+                        '.',
+                        style: TextStyle(
+                            color: Colors.green[400],
+                            fontSize: 80.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'EMAIL',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'PASSWORD',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            alignment: Alignment(1.0, 0),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+              child: InkWell(
+                child: Text(
+                  "Forgot password",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 5, 20.0, 5),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  MaterialButton(
+                    onPressed: () {},
+                    elevation: 0.0,
+                    minWidth: MediaQuery.of(context).size.width,
+                    color: Colors.green,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  MaterialButton(
+                    onPressed: () {},
+                    elevation: 0.5,
+                    minWidth: MediaQuery.of(context).size.width,
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.fingerprint,
+                        color: Colors.black,
+                      ),
+                      title: Text('Login with Facebook'),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0,),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Need to spotify?",
+                  style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                      child: Text(" Register",style: TextStyle(color: Colors.green),))
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0,)
+        ],
+      ),
+    );
+  }
+}
